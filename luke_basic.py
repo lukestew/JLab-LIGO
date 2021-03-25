fe81f47ae1abbcd091606fba661789876cb38a06
lukestew <lukestew@mit.edu>
2021-03-25 11:48:23 -0400

    Updates to full python script

--- luke_basic.py
+++ luke_basic.py
@@ -5,9 +5,9 @@ Created on Thu Mar 25 09:51:40 2021
 
 @author: lukestew
 """
-#----------------------------------------------------------------
+# ----------------------------------------------------------------
 # Load libraries
-#----------------------------------------------------------------
+# ----------------------------------------------------------------
 import numpy as np
 import math
 from gwpy.timeseries import TimeSeries
@@ -19,49 +19,45 @@ import lmfit
 
 import os
 
+
 # define osc_dif for lmfit::minimize()
 def osc_dif(params, x, data, eps):
-    iM=params["Mc"]
-    iT0=params["t0"]
-    norm=params["C"]
-    phi=params["phi"]
-    val=osc(x, iM, iT0, norm, phi)
-    return (val-data)/eps
+    iM = params["Mc"]
+    iT0 = params["t0"]
+    norm = params["C"]
+    phi = params["phi"]
+    val = osc(x, iM, iT0, norm, phi)
+    return (val - data) / eps
 
 
-
-#----------------------------------------------------------------
+# ----------------------------------------------------------------
 # Set parameters
-#----------------------------------------------------------------
-fn = 'data/H-H1_GWOSC_4KHZ_R1-1126257415-4096.hdf5' # data file
-tevent = 1126259462.422 # Mon Sep 14 09:50:45 GMT 2015
-evtname = 'GW150914' # event name
+# ----------------------------------------------------------------
+fn = 'data/H-H1_GWOSC_4KHZ_R1-1126257415-4096.hdf5'  # data file
+tevent = 1126259462.422  # Mon Sep 14 09:50:45 GMT 2015
+evtname = 'GW150914'  # event name
 
-detector = 'L1' # detecotr: L1 or H1
+detector = 'L1'  # detecotr: L1 or H1
 
-
-#----------------------------------------------------------------
+# ----------------------------------------------------------------
 # Load LIGO data
-#----------------------------------------------------------------
+# ----------------------------------------------------------------
 strain = TimeSeries.read(fn, format='hdf5.losc')
 center = int(tevent)
-strain = strain.crop(center-16, center+16)
+strain = strain.crop(center - 16, center + 16)
 
-
-
-#----------------------------------------------------------------
+# ----------------------------------------------------------------
 # Show LIGO strain vs. time
-#----------------------------------------------------------------
+# ----------------------------------------------------------------
 plt.figure()
 strain.plot()
 plt.ylabel('strain')
-plt.show()
+# plt.show()
 
 
-
-#----------------------------------------------------------------
+# ----------------------------------------------------------------
 # Obtain the power spectrum density PSD / ASD
-#----------------------------------------------------------------
+# ----------------------------------------------------------------
 
 asd = strain.asd(fftlength=8)
 
@@ -71,26 +67,23 @@ plt.xlim(10, 2000)
 plt.ylim(1e-24, 1e-19)
 plt.ylabel('ASD (strain/Hz$^{1/2})$')
 plt.xlabel('Frequency (Hz)')
-plt.show()
+# plt.show()
 
 
-
-
-#----------------------------------------------------------------
+# ----------------------------------------------------------------
 # Whitening data
-#----------------------------------------------------------------
+# ----------------------------------------------------------------
 
 white_data = strain.whiten()
 
 plt.clf()
 white_data.plot()
 plt.ylabel('strain (whitened)')
-plt.show()
 
 
-#----------------------------------------------------------------
+# ----------------------------------------------------------------
 # Bandpass filtering
-#----------------------------------------------------------------
+# ----------------------------------------------------------------
 
 bandpass_low = 80
 bandpass_high = 180
@@ -102,25 +95,27 @@ white_data_bp.plot()
 plt.ylabel('strain (whitened + band-pass)')
 
 plt.figure()
-#--------------------------------------------
+# --------------------------------------------
 # Zoom in on signal
 # -------------------------------------------
 plt.clf()
 white_data_bp.plot()
 plt.ylabel('strain (whitened + band-pass)')
-plt.xlim(tevent-.17, tevent+.13)
-plt.show()
+plt.xlim(tevent - .17, tevent + .13)
 
 
+# plt.show()
 
-#----------------------------------------------------------------
+
+# ----------------------------------------------------------------
 # Frequency analytic
-#----------------------------------------------------------------
+# ----------------------------------------------------------------
 
-def gwfreq(iM,iT,iT0):
-    const = (948.5)*np.power((1./iM),5./8.)
-    output = const*np.power(np.maximum((iT0-iT),3e-2),-3./8.) # we can max it out above 500 Hz-ish
+def gwfreq(iM, iT, iT0):
+    const = (948.5) * np.power((1. / iM), 5. / 8.)
+    output = const * np.power(np.maximum((iT0 - iT), 3e-2), -3. / 8.)  # we can max it out above 500 Hz-ish
     return output
+
 
 times = np.linspace(0., 4., 50)
 freq = gwfreq(20, times, 4)
@@ -129,23 +124,27 @@ plt.clf()
 plt.plot(times, freq)
 plt.xlabel('Time (s)')
 plt.ylabel('Frequency (Hz)')
-plt.show()
 
 
+# plt.show()
 
-#----------------------------------------------------------------
+
+# ----------------------------------------------------------------
 # Model fit function
-#----------------------------------------------------------------
+# ----------------------------------------------------------------
 
 def osc(t, Mc, t0, C, phi):
     vals = []
     for time in t:
         if time <= t0:
-            vals.append(C*1e-12 * (Mc * gwfreq(Mc, time, t0))**(10/3) * np.cos(gwfreq(Mc, time, t0) * (time-t0) + phi))
+            vals.append(
+                C * 1e-12 * (Mc * gwfreq(Mc, time, t0)) ** (10 / 3) * np.cos(gwfreq(Mc, time, t0) * (time - t0) + phi))
         else:
-            vals.append(np.exp(-100*(time-t0)) * C*1e-12 * (Mc * gwfreq(Mc, time, t0))**(10/3) * np.cos(gwfreq(Mc, time, t0) * (time-t0) + phi))
+            vals.append(np.exp(-100 * (time - t0)) * C * 1e-12 * (Mc * gwfreq(Mc, time, t0)) ** (10 / 3) * np.cos(
+                gwfreq(Mc, time, t0) * (time - t0) + phi))
 
     return np.asarray(vals)
+
 
 # Draw the function defined
 times = np.linspace(-0.1, 0.3, 1000)
@@ -154,25 +153,24 @@ omega = gwfreq(30, times, .18)
 plt.figure(figsize=(12, 4))
 plt.subplots_adjust(left=0.1, right=0.9, top=0.85, bottom=0.2)
 plt.plot(times, freq)
-plt.xlabel('Time (s) since '+str(tevent))
+plt.xlabel('Time (s) since ' + str(tevent))
 plt.ylabel('strain')
 
 plt.figure(figsize=(12, 4))
 plt.plot(times, omega)
 
-plt.show()
+# plt.show()
 
 
-
-#----------------------------------------------------------------
+# ----------------------------------------------------------------
 # Fit
-#----------------------------------------------------------------
+# ----------------------------------------------------------------
 
 sample_times = white_data_bp.times.value
 sample_data = white_data_bp.value
-indxt = np.where((sample_times >= (tevent-0.17)) & (sample_times < (tevent+0.13)))
+indxt = np.where((sample_times >= (tevent - 0.17)) & (sample_times < (tevent + 0.13)))
 x = sample_times[indxt]
-x = x-x[0]
+x = x - x[0]
 white_data_bp_zoom = sample_data[indxt]
 
 plt.figure(figsize=(12, 4))
@@ -186,25 +184,13 @@ from lmfit import Model, minimize, fit_report, Parameters
 
 model = lmfit.Model(osc)
 p = model.make_params()
-p['Mc'].set(20)     # Mass guess
+p['Mc'].set(20)  # Mass guess
 p['t0'].set(0.18)  # By construction we put the merger in the center
-p['C'].set(1)      # normalization guess
-p['phi'].set(0)    # Phase guess
-unc = np.full(len(white_data_bp_zoom),20)
+p['C'].set(1)  # normalization guess
+p['phi'].set(0)  # Phase guess
+unc = np.full(len(white_data_bp_zoom), 20)
 out = minimize(osc_dif, params=p, args=(x, white_data_bp_zoom, unc))
 print(fit_report(out))
-plt.plot(x, model.eval(params=out.params,t=x),'r',label='best fit')
+plt.plot(x, model.eval(params=out.params, t=x), 'r', label='best fit')
+# plt.show()
 plt.show()
-
-
-
-
-
-
-
-
-
-
-
-
-
